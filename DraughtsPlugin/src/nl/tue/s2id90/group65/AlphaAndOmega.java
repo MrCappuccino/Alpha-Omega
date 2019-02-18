8b37c553d226ea8330a8a0db1adf245ed1b10796
niklas <niklas@stylianou.info>
2019-02-18 01:44:28 +0100

    iterative deepening, will now always find a non-random move

--- DraughtsPlugin/src/nl/tue/s2id90/group65/AlphaAndOmega.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group65/AlphaAndOmega.java
@@ -15,37 +15,33 @@ import org10x10.dam.game.Move;
 public class AlphaAndOmega  extends DraughtsPlayer {
 
     private int bestValue = 0;
-    int maxSearchDepth;
 
     /** boolean that indicates that the GUI asked the player to stop thinking. */
     private boolean stopped;
 
-    public AlphaAndOmega(int maxSearchDepth) {
+    public AlphaAndOmega() {
         super("best.png"); // ToDo: replace with your own icon
-        this.maxSearchDepth = maxSearchDepth;
     }
 
     @Override
     public Move getMove(DraughtsState s) {
         Move bestMove = null;
-        bestValue = 0;
         DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
 
         try {
             // compute bestMove and bestValue in a call to alphabeta
-            bestValue = alphaBeta(node, MIN_VALUE, MAX_VALUE, maxSearchDepth);
-
-            // store the bestMove found uptill now
-            // NB this is not done in case of an AIStoppedException in alphaBeat()
-            bestMove  = node.getBestMove();
+            alphaBeta(node, MIN_VALUE, MAX_VALUE);
 
             // print the results for debugging reasons
             System.err.format(
-                    "%s: depth= %2d, best move = %5s, value=%d\n", 
-                    this.getClass().getSimpleName(),maxSearchDepth, bestMove, bestValue
+                    "%s: best move = %5s, value=%d\n", 
+                    this.getClass().getSimpleName(), bestMove, bestValue
                     );
         } catch (AIStoppedException ex) {  /* nothing to do */  }
-
+		
+		bestMove = node.getBestMove();		// store best recent move
+		System.out.println("bestFinalMove: " + bestMove);
+		
         if (bestMove == null) {
             System.err.println("no valid move found!");
             return getRandomValidMove(s);
@@ -87,13 +83,16 @@ public class AlphaAndOmega  extends DraughtsPlayer {
      * @return the computed value of this node
      * @throws AIStoppedException
      **/
-    int alphaBeta(DraughtsNode node, int alpha, int beta, int depth) throws AIStoppedException
+    void alphaBeta(DraughtsNode node, int alpha, int beta) throws AIStoppedException
     {
-        if (node.getState().isWhiteToMove()) {
-            return alphaBetaMax(node, alpha, beta, depth);
-        } else  {
-            return alphaBetaMin(node, alpha, beta, depth);
-        }
+		for  (int depth = 1;; depth++) {
+			if (node.getState().isWhiteToMove()) {
+				bestValue = alphaBetaMax(node, alpha, beta, depth);
+			} else  {
+				bestValue = alphaBetaMin(node, alpha, beta, depth);
+			}
+			System.out.println("bestMove: " + node.getBestMove() + " at depth: " + depth);
+		}
     }
 
     /** Does an alphabeta computation with the given alpha and beta
@@ -210,7 +209,6 @@ public class AlphaAndOmega  extends DraughtsPlayer {
           */
 
         int total = 0;
-        state.
 
             if (state.isEndState()) { // Leaf
                 if (state.isWhiteToMove()) {
